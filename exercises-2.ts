fed5ba1c2f5089c1f945e4da94b276c6007da0ee
Lean <leandrodiaz042@gmail.com>
2024-01-11 16:56:03 -0300

    Exercises 2

---
+++ exercises-2.ts
@@ -0,0 +1,62 @@
+// 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+// 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+// 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+// 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+// Exercise 5
+const revertText = (text: string = "") =>
+  !text
+    ? console.warn("You must enter text")
+    : console.log(text.split("").reverse().join(""));
+
+revertText("");
+revertText("Hola como estas");
+
+// Exercise 6
+const countWords = (text: string, word: string) => {
+  if (!text) return console.warn("You must enter text");
+  if (!word) return console.warn("You must enter the word to count");
+  const separatedWords = text.split(" ");
+  const timesInText = separatedWords.filter(words => words === word);
+  console.log(timesInText.length);
+};
+
+countWords("", "pollo");
+countWords("", "pollo");
+countWords("pollo arbejas pollo pollo", "pollo");
+
+// Exercise 7
+const isPalindromes = (text: string) => {
+  if (!text) return console.warn("You must enter text");
+  const reversed = text.split("").reverse().join("");
+  console.log(reversed);
+  console.log(text);
+  console.log(text == reversed);
+};
+
+isPalindromes("salas");
+isPalindromes("dog");
+isPalindromes("545");
+isPalindromes("25464");
+
+// now with input of type number
+const isPalindromesNum = (numero: number) => {
+  const text = numero.toString();
+  if (!text) return console.warn("You must enter text");
+  const reversed = text.split("").reverse().join("");
+  console.log(reversed);
+  console.log(text);
+  console.log(text == reversed);
+};
+
+isPalindromesNum(234);
+isPalindromesNum(404);
+
+// Exercise 8
+const deletePath = (text: string, path: string) => {
+  if (!text) return console.warn("You must enter text");
+  console.log(text.replaceAll(path, "").trim());
+};
+
+deletePath("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+deletePath("monochromatic, monopolio, monopatin mono", "mono");
