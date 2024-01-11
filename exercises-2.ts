8d11bb02b663692bf54ce6ad7cfdee74b0c13e4d
Lean <leandrodiaz042@gmail.com>
2024-01-11 17:00:59 -0300

    Reformated code

--- exercises-2.ts
+++ exercises-2.ts
@@ -16,9 +16,7 @@ revertText("Hola como estas");
 const countWords = (text: string, word: string) => {
   if (!text) return console.warn("You must enter text");
   if (!word) return console.warn("You must enter the word to count");
-  const separatedWords = text.split(" ");
-  const timesInText = separatedWords.filter(words => words === word);
-  console.log(timesInText.length);
+  console.log(text.split(" ").filter(words => words === word).length);
 };
 
 countWords("", "pollo");
@@ -26,13 +24,10 @@ countWords("", "pollo");
 countWords("pollo arbejas pollo pollo", "pollo");
 
 // Exercise 7
-const isPalindromes = (text: string) => {
-  if (!text) return console.warn("You must enter text");
-  const reversed = text.split("").reverse().join("");
-  console.log(reversed);
-  console.log(text);
-  console.log(text == reversed);
-};
+const isPalindromes = (text: string) =>
+  !text
+    ? console.warn("You must enter text")
+    : console.log(text == text.split("").reverse().join(""));
 
 isPalindromes("salas");
 isPalindromes("dog");
@@ -42,21 +37,19 @@ isPalindromes("25464");
 // now with input of type number
 const isPalindromesNum = (numero: number) => {
   const text = numero.toString();
-  if (!text) return console.warn("You must enter text");
-  const reversed = text.split("").reverse().join("");
-  console.log(reversed);
-  console.log(text);
-  console.log(text == reversed);
+  !text
+    ? console.warn("You must enter text")
+    : console.log(text == text.split("").reverse().join(""));
 };
 
 isPalindromesNum(234);
 isPalindromesNum(404);
 
 // Exercise 8
-const deletePath = (text: string, path: string) => {
-  if (!text) return console.warn("You must enter text");
-  console.log(text.replaceAll(path, "").trim());
-};
+const deletePath = (text: string, path: string) =>
+  !text
+    ? console.warn("You must enter text")
+    : console.log(text.replaceAll(path, "").trim());
 
 deletePath("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
 deletePath("monochromatic, monopolio, monopatin mono", "mono");
