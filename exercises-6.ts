3200a4f2ff7805d36168235f26fa344f20f784ab
Leandro Diaz <leandrodiaz042@gmail.com>
2024-03-20 16:50:32 -0300

    add exercises done today

--- exercises-6.ts
+++ exercises-6.ts
@@ -1,3 +1,21 @@
-// 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
-// 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
-// 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+// Exercise 18 - Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+
+let vocales = ["a", "e", "i", "o", "u"]
+
+function contarVocales(word: string) {
+  let voc = 0,
+    con = 0
+  word
+    .trim()
+    .split("")
+    .forEach(letter => {
+      if (vocales.includes(letter)) {
+        voc++
+      } else {
+        con++
+      }
+    })
+  return console.log(`En la palabra ${word} hay => Vocales: ${voc}, Consonantes: ${con}`)
+}
+
+contarVocales("hola mundo")
---
+++ exercises-7.ts
@@ -0,0 +1,15 @@
+/* Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+####### 
+*/
+
+for (let i = "#"; i <= "#######"; i += "#") {
+  console.log(i)
+}
